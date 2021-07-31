b8e86e91081f789f01987e34e912e73939fb6d4d
Sathia <sathiasara@gmail.com>
2021-07-31 12:46:52 +0100

    Pipeline 1.1.3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -e
 
-echo Namespace = "$1"
-echo Releasename = "$2"
 echo "----------------------Installing pages---------------------------------"
 
-RELEASE_NAME="$2"
+RELEASE_NAME=${{ secrets.RELEASE_NAME }} 
 
-NAMESPACE="$1"
+NAMESPACE=${{ secrets.STAGING_NAMESPACE }}
 
 
 kubectl get ns "$NAMESPACE" &> /dev/null || kubectl create ns "$NAMESPACE"
 
-helm upgrade --install "$RELEASE_NAME" pages -n "$NAMESPACE" --debug
+   
+
+helm upgrade --install $RELEASE_NAME pages -n $NAMESPACE --debug
 helm list
 kubectl config current-context
 sleep 30s
@@ -20,4 +20,4 @@ sleep 30s
 kubectl get pods -n "$NAMESPACE"
 
 
-echo "----------------------Completed Installation of  pages------------------"
+echo "----------------------Completed Installation of pages------------------"
